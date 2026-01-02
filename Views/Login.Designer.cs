dc161cb68cf6d631e3b56258e818d72a9485d25f
pphatdev <leatsophat02@gmail.com>
2026-01-02 20:17:33 +0700

    Add logo asset and update resource files for logo integration in Login form

--- Views/Login.Designer.cs
+++ Views/Login.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             label1 = new Label();
             label4 = new Label();
             textBoxUsername = new RoundedTextBox();
@@ -37,11 +36,7 @@
             Logo = new PictureBox();
             LoginButton = new RoundedButton();
             ExitButton = new RoundedButton();
-
             ((System.ComponentModel.ISupportInitialize)Logo).BeginInit();
-
-
-
             SuspendLayout();
             // 
             // label1
@@ -57,9 +52,80 @@
             label1.Text = "POS System";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // label4
+            // 
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ActiveCaptionText;
+            label4.Location = new Point(265, 290);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(149, 31);
+            label4.TabIndex = 40;
+            label4.Text = "Username";
+            // 
+            // textBoxUsername
+            // 
+            textBoxUsername.BackColor = Color.White;
+            textBoxUsername.BorderColor = Color.Gray;
+            textBoxUsername.BorderFocusColor = Color.SkyBlue;
+            textBoxUsername.BorderRadius = 10;
+            textBoxUsername.BorderSize = 2;
+            textBoxUsername.Font = new Font("Kantumruy Pro Medium", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBoxUsername.ForeColor = Color.DimGray;
+            textBoxUsername.Location = new Point(270, 327);
+            textBoxUsername.Margin = new Padding(4, 5, 4, 5);
+            textBoxUsername.Name = "textBoxUsername";
+            textBoxUsername.Padding = new Padding(12, 8, 12, 8);
+            textBoxUsername.PasswordChar = false;
+            textBoxUsername.Size = new Size(380, 46);
+            textBoxUsername.TabIndex = 1;
+            textBoxUsername.UnderlinedStyle = false;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(266, 378);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(149, 26);
+            label2.TabIndex = 46;
+            label2.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.BackColor = Color.White;
+            textBoxPassword.BorderColor = Color.Gray;
+            textBoxPassword.BorderFocusColor = Color.SkyBlue;
+            textBoxPassword.BorderRadius = 10;
+            textBoxPassword.BorderSize = 2;
+            textBoxPassword.Font = new Font("Kantumruy Pro Medium", 11.999999F, FontStyle.Bold);
+            textBoxPassword.ForeColor = Color.DimGray;
+            textBoxPassword.Location = new Point(270, 409);
+            textBoxPassword.Margin = new Padding(4, 5, 4, 5);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Padding = new Padding(12, 8, 12, 8);
+            textBoxPassword.PasswordChar = false;
+            textBoxPassword.Size = new Size(380, 46);
+            textBoxPassword.TabIndex = 2;
+            textBoxPassword.UnderlinedStyle = false;
+            // 
+            // Logo
+            // 
+            Logo.Image = Properties.Resources.logo;
+            Logo.Location = new Point(392, 60);
+            Logo.Name = "Logo";
+            Logo.Size = new Size(125, 125);
+            Logo.SizeMode = PictureBoxSizeMode.StretchImage;
+            Logo.TabIndex = 47;
+            Logo.TabStop = false;
+            // 
             // LoginButton
             // 
             LoginButton.BackColor = Color.FromArgb(68, 189, 50);
+            LoginButton.BorderColor = Color.PaleVioletRed;
+            LoginButton.BorderRadius = 10;
+            LoginButton.BorderSize = 0F;
             LoginButton.FlatStyle = FlatStyle.Flat;
             LoginButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             LoginButton.ForeColor = Color.White;
@@ -70,53 +136,15 @@
             LoginButton.TabIndex = 3;
             LoginButton.Text = "Login";
             LoginButton.UseVisualStyleBackColor = false;
-
-            // 
-            // label4
-            // 
-            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.ForeColor = SystemColors.ActiveCaptionText;
-            label4.Location = new Point(276, 290);
-            label4.Margin = new Padding(4, 0, 4, 0);
-            label4.Name = "label4";
-            label4.Size = new Size(149, 31);
-            label4.TabIndex = 40;
-            label4.Text = "Username";
-            // 
-            // textBoxUsername
-            // 
-            textBoxUsername.Font = new Font("Kantumruy Pro Medium", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            textBoxUsername.Location = new Point(281, 326);
-            textBoxUsername.Margin = new Padding(4, 5, 4, 5);
-            textBoxUsername.Name = "textBoxUsername";
-            textBoxUsername.Size = new Size(351, 31);
-            textBoxUsername.TabIndex = 1;
-            // 
-            // label2
-            // 
-            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.ForeColor = SystemColors.ActiveCaptionText;
-            label2.Location = new Point(276, 378);
-            label2.Margin = new Padding(4, 0, 4, 0);
-            label2.Name = "label2";
-            label2.Size = new Size(149, 26);
-            label2.TabIndex = 46;
-            label2.Text = "Password";
-            // 
-            // textBoxPassword
-            // 
-            textBoxPassword.Font = new Font("Kantumruy Pro Medium", 11.999999F, FontStyle.Bold);
-            textBoxPassword.Location = new Point(281, 409);
-            textBoxPassword.Margin = new Padding(4, 5, 4, 5);
-            textBoxPassword.Name = "textBoxPassword";
-            textBoxPassword.Size = new Size(351, 31);
-            textBoxPassword.TabIndex = 2;
             // 
             // ExitButton
             // 
             ExitButton.BackColor = Color.FromArgb(192, 0, 0);
+            ExitButton.BorderColor = Color.PaleVioletRed;
+            ExitButton.BorderRadius = 10;
+            ExitButton.BorderSize = 0F;
             ExitButton.FlatStyle = FlatStyle.Flat;
-            ExitButton.Font = new Font("Kantumruy Pro Medium", 11.999999F, FontStyle.Bold);
+            ExitButton.Font = new Font("Kantumruy Pro Medium", 12F, FontStyle.Bold);
             ExitButton.ForeColor = Color.White;
             ExitButton.Location = new Point(400, 470);
             ExitButton.Margin = new Padding(4, 5, 4, 5);
@@ -126,16 +154,6 @@
             ExitButton.Text = "Exit";
             ExitButton.UseVisualStyleBackColor = false;
             ExitButton.Click += OnExit;
-            // 
-            // Logo
-            // 
-            Logo.Image = (Image)resources.GetObject("Logo.Image");
-            Logo.Location = new Point(392, 60);
-            Logo.Name = "Logo";
-            Logo.Size = new Size(125, 125);
-            Logo.SizeMode = PictureBoxSizeMode.StretchImage;
-            Logo.TabIndex = 47;
-            Logo.TabStop = false;
             // 
             // LoginForm
             // 
@@ -158,7 +176,6 @@
             StartPosition = FormStartPosition.CenterScreen;
             ((System.ComponentModel.ISupportInitialize)Logo).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
